b39281add2948c609c037e5d2d29e0049016eb6e
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-16 11:53:25 +0300

    Ideju Zara ideja cuh

--- src/gitty/Kaulins.java
+++ src/gitty/Kaulins.java
@@ -3,13 +3,14 @@ package gitty;
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	static void mesties() {
 		int sk;
 		Random rng = new Random();
 		sk = rng.nextInt(1)+6;
 		System.out.print("Tu uzmeti "+sk);
-
+	}
+	public static void main(String[] args) {
+		mesties();
 	}
 
 }
