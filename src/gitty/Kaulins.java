26266b9196a1ebe072a480c4a201d918d916698e
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-16 11:44:03 +0300

    Izmaiņas cuh

--- src/gitty/Kaulins.java
+++ src/gitty/Kaulins.java
@@ -1,9 +1,14 @@
 package gitty;
 
+import java.util.Random;
+
 public class Kaulins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int sk;
+		Random rng = new Random();
+		sk = rng.nextInt(1)+6;
+		System.out.print("Tu uzmeti "+sk);
 
 	}
 
