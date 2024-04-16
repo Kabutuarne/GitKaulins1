9926b33d409fc2f1af08a82930c4addfad5d672e
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-16 12:02:13 +0300

    Main izmaiņas cuh

--- src/gitty/Kaulins.java
+++ src/gitty/Kaulins.java
@@ -1,15 +1,20 @@
 package gitty;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int sk;
+		int sk, reizes;
 		Random rng = new Random();
-		sk = rng.nextInt(1)+6;
-		System.out.print("Tu uzmeti "+sk);
-
+		Scanner dati = new Scanner(System.in);
+		System.out.print("Cik reizes metīsi kauliņu?: ");
+		reizes = dati.nextInt();
+		for(int i=0;i<reizes;i++) {
+		sk = rng.nextInt(6)+1;
+		System.out.println("Tu uzmeti "+sk);
+		}
 	}
 
 }
