4dc7d41dbac408238eb6c660eb6e7f474bf65a28
unknown <martins.lagzdins@sk.lvt.lv>
2024-04-16 11:37:25 +0300

    Sākums cuh

---
+++ src/gitty/Kaulins.java
@@ -0,0 +1,10 @@
+package gitty;
+
+public class Kaulins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitKaulins1 {
+}
