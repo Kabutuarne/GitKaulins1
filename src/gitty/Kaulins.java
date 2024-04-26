836b243d6677c3c66dc536b484bbf43a58b25f58
Lagzdiņš <martins.lagzdins@sk.lvt.lv>
2024-04-26 08:16:10 +0300

    Pievienots ierobežojums reižu ievadei

--- src/gitty/Kaulins.java
+++ src/gitty/Kaulins.java
@@ -4,30 +4,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Kaulins {
-<<<<<<< HEAD
-
+static void mesties(int reizes) {
+	int sk;
+	for(int i=0;i<reizes;i++) {
+	Random rng = new Random();
+	sk = rng.nextInt(6)+1;
+	System.out.println("Tu uzmeti "+sk);
+	}
+}
 	public static void main(String[] args) {
-		int sk, reizes;
+		int reizes;
 		Random rng = new Random();
 		Scanner dati = new Scanner(System.in);
+		do {
 		System.out.print("Cik reizes metīsi kauliņu?: ");
 		reizes = dati.nextInt();
-		for(int i=0;i<reizes;i++) {
-		sk = rng.nextInt(6)+1;
-		System.out.println("Tu uzmeti "+sk);
-		}
-=======
-	static void mesties(int reizes) {
-		int sk;
-		for(int i=0;i<reizes;i++) {
-		Random rng = new Random();
-		sk = rng.nextInt(6)+1;
-		System.out.println("Tu uzmeti "+sk);
-		}
-	}
-	public static void main(String[] args) {
-		mesties(4);
->>>>>>> idejuZARS
+		}while(reizes<1);
+		mesties(reizes);
 	}
 
 }
